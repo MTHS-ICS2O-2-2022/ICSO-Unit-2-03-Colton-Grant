92f032ef8b063331902c1a585d5c955e58545739
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-03-20 17:16:11 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const streetname = document.getElementById("street-name").value
+  const addressnumber = parseInt(document.getElementById("number-entered").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "your address is: " + streetname + ", " + addressnumber+ "."
+}
